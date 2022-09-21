cc7284e74231e78fe603d3ce1f33d0d0c1c5ba36
mariogodino <110245361+mariogodino@users.noreply.github.com>
2022-09-21 11:42:05 -0300

    Prueba
    Prueba 2- Syso mario

--- Proyecto/src/main/Main.java
+++ Proyecto/src/main/Main.java
@@ -16,6 +16,7 @@ public class Main {
 		
 		System.out.println(handler.listaDePersonas);
 		
+		System.out.println("Prueba Mario");
 		
 		InterfazPrincipal principalFrame = new InterfazPrincipal(handler);
 		principalFrame.setVisible(true);
