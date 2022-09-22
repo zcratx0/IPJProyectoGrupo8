a8e9b6c87153d7a76198cf3981bedcdeff94c2a2
mariogodino <110245361+mariogodino@users.noreply.github.com>
2022-09-22 11:53:23 -0300

    Agregando Dashboard
    Creé una nueva clase InterfazDashboard dentro de Swing. Toy trabajando en ella.

---
+++ Proyecto/src/swing/InterfazDashboard.java
@@ -0,0 +1,31 @@
+package swing;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import clases.ExportExcel;
+import clases.ListaPersonaHandler;
+import clases.Persona;
+
+import java.awt.List;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.Color;
+import java.awt.Image;
+
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+
+public class InterfazDashboard extends JFrame {
+
+}
