5f73c35e2f4cf9d9a1349686a7f32508e720580d
mariogodino <110245361+mariogodino@users.noreply.github.com>
2022-09-22 13:29:38 -0300

    Update InterfazDashboard.java

--- Proyecto/src/swing/InterfazDashboard.java
+++ Proyecto/src/swing/InterfazDashboard.java
@@ -25,7 +25,53 @@ import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import java.awt.Font;
 import javax.swing.SwingConstants;
+import java.awt.BorderLayout;
 
 public class InterfazDashboard extends JFrame {
-
+	public InterfazDashboard() {
+	
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JPanel panelJpanel = new JPanel();
+		panelJpanel.setLayout(null);
+		getContentPane().add(panelJpanel, BorderLayout.CENTER);
+		
+		JLabel tituloJLabel = new JLabel("Dashboard");
+		tituloJLabel.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		tituloJLabel.setBounds(223, 11, 105, 33);
+		panelJpanel.add(tituloJLabel);
+		
+		JLabel cantidadDePersonasJLabel = new JLabel("Cantidad de personas ingresadas : ");
+		cantidadDePersonasJLabel.setFont(new Font("Tahoma", Font.ITALIC, 18));
+		cantidadDePersonasJLabel.setBounds(10, 78, 290, 33);
+		panelJpanel.add(cantidadDePersonasJLabel);
+		
+		JLabel numeroPersonasJLabel = new JLabel("0");
+		numeroPersonasJLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		numeroPersonasJLabel.setBounds(338, 77, 105, 33);
+		panelJpanel.add(numeroPersonasJLabel);
+		
+		JLabel promedioDeVehiculosJLabel = new JLabel("Promedio de vehículos por persona : ");
+		promedioDeVehiculosJLabel.setFont(new Font("Tahoma", Font.ITALIC, 18));
+		promedioDeVehiculosJLabel.setBounds(10, 134, 318, 33);
+		panelJpanel.add(promedioDeVehiculosJLabel);
+		
+		JLabel numeroPromedioJLabel = new JLabel("0");
+		numeroPromedioJLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		numeroPromedioJLabel.setBounds(338, 133, 105, 33);
+		panelJpanel.add(numeroPromedioJLabel);
+		
+		JButton botonGraficarBtn = new JButton("Graficar vehículos por depto.");
+		botonGraficarBtn.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				
+				
+				
+			}
+		});
+		botonGraficarBtn.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		botonGraficarBtn.setBounds(150, 228, 262, 40);
+		panelJpanel.add(botonGraficarBtn);
+		
+	}
 }
